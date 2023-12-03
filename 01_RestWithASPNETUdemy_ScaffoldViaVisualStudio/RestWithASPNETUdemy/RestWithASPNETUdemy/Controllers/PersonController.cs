d5222cd02dbb57b4d1d5ea5677a52aab98a077df
GuilhermePereira91 <guihermexmarques91@gmail.com>
2023-12-03 19:43:19 -0300

    trabalhando com os 4 verbos

---
+++ 01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Ivalid Input");
+        }
+    }
+}
